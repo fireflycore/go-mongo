5ba9b1d77eed96f1b5cf9cb9c041ec5f20426e2a
lhdht <15229684090@163.com>
2026-01-08 16:58:30 +0800

    feature: uuid

---
+++ uuid.go
@@ -0,0 +1,12 @@
+package mongo
+
+import "github.com/google/uuid"
+
+// NewUUIDv7 生成一个 UUIDv7 字符串
+func NewUUIDv7() string {
+	u, e := uuid.NewV7()
+	if e != nil {
+		return NewUUIDv7()
+	}
+	return u.String()
+}
